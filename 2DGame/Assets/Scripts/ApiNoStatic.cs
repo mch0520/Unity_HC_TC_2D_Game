ce76b2596af8fcfd8f5939ac3368b74ef4dec7a4
mch0520 <ch052088@gmail.com>
2020-12-17 21:36:10 +0800

    Instantiate生成方塊與取得下個方塊

---
+++ 2DGame/Assets/Scripts/ApiNoStatic.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ApiNoStatic : MonoBehaviour
+{
+    //使用一班屬性
+    //類型 欄位名稱
+    public Transform tran1;
+    public Transform tran2;
+
+    public GameObject myGO;
+
+    public Transform tra;
+
+    public void start()
+    {
+        //一般屬性
+        //取得
+        //語法:
+        //類型的欄位名稱 的 一班屬性
+        print("物件1的座標"+tran1);
+        //設定
+        //語法:
+        //類型欄位名稱 的 一班屬性 = 對應的值
+        tran2.position = new Vector3(2,6,7);
+
+        print("我的遊戲物件圖曾為:"+myGO.layer);
+
+        myGO.layer = 4;
+
+        //一般方法
+        //使用
+        //語法:
+        //類型欄位名稱 的 方法(對應的參數)
+        tra.Rotate(0,0,5);
+        tra.Translate(0,2,0);
+
+    }
+
+
+
+
+
+
+
+}
--- 2DGame/Assets/Scripts/TetrisManager.cs
+++ 2DGame/Assets/Scripts/TetrisManager.cs
@@ -6,14 +6,14 @@ using UnityEngine;
 public class TetrisManager : MonoBehaviour
 {
     #region 控制器欄位
-    [Header("掉落速度"),Range(0.1f,3)]
-    public float downtime=1.5f;
+    [Header("掉落速度"), Range(0.1f, 3)]
+    public float downtime = 1.5f;
     [Header("目前成績")]
     public int score;
     [Header("最高成績")]
     public int Hscore;
     [Header("等級")]
-    public int level=1;
+    public int level = 1;
     [Header("結束畫面")]
     public GameObject endimage;
     [Header("音效:掉落、移動、清除、結束")]
@@ -21,11 +21,57 @@ public class TetrisManager : MonoBehaviour
     public AudioClip move;
     public AudioClip clear;
     public AudioClip end;
+    [Header("下一個方塊")]
+    public Transform nextGoArea;
+    [Header("下一個方塊的編號")]
+    public int nextIndex;
+    [Header("畫布")]
+    public Transform traCanvas;
+
     #endregion
+    public void Start()
+    {
+        RandomTetris();
+
+    }
+
+
+
+    private void RandomTetris()
+    {
+        //整數不會等於最大值
+        nextIndex = Random.Range(0, 7);
+        //下一個俄羅斯方塊範圍，取得子物件(子物件編號)，轉為遊戲物件，啟動設定(顯示)
+        nextGoArea.GetChild(nextIndex).gameObject.SetActive(true);
+    }
+    /// <summary>
+    /// 開始遊戲
+    /// 1.生成俄羅斯方塊放在正確位置
+    /// 2.上一次俄羅斯方塊要隱藏
+    /// 3.隨機取得下一個
+    /// </summary>
+    public void StarGame()
+    {
+        //保存上一次的方塊
+        GameObject tetris = nextGoArea.GetChild(nextIndex).gameObject;
+        //生成物件(生成物件，父物件)
+        GameObject current = Instantiate(tetris, traCanvas);
+        //GetComponent<任何元件>()
+        //<>    指所有類型
+        current.GetComponent<RectTransform>().anchoredPosition = new Vector2(-15,360);
+
+        //上一個方塊隱藏
+        tetris.SetActive(false);
+        //隨機取得下一個方塊
+        RandomTetris();
+
+    }
 
     private void AddGo()
     {
-        return ;
+        
+
+
 
     }
 
