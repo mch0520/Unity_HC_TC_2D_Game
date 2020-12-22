91e746520846996b10995db334f2de8daf91d0c0
mch0520 <ch052088@gmail.com>
2020-12-22 20:41:50 +0800

    控制方塊移動與旋轉

--- 2DGame/Assets/Scripts/TetrisManager.cs
+++ 2DGame/Assets/Scripts/TetrisManager.cs
@@ -7,7 +7,7 @@ public class TetrisManager : MonoBehaviour
 {
     #region 控制器欄位
     [Header("掉落速度"), Range(0.1f, 3)]
-    public float downtime = 1.5f;
+    public float timeFall = 1.5f;
     [Header("目前成績")]
     public int score;
     [Header("最高成績")]
@@ -23,10 +23,21 @@ public class TetrisManager : MonoBehaviour
     public AudioClip end;
     [Header("下一個方塊")]
     public Transform nextGoArea;
-    [Header("下一個方塊的編號")]
-    public int nextIndex;
+
+    /// <summary>
+    /// 下一個方塊的編號
+    /// </summary>
+    private int nextIndex;
     [Header("畫布")]
     public Transform traCanvas;
+    /// <summary>
+    /// 目前俄羅斯方塊
+    /// </summary>
+    public RectTransform currentTetris;
+    /// <summary>
+    /// 計時器
+    /// </summary>
+    private float timer;
 
     #endregion
     public void Start()
@@ -35,7 +46,50 @@ public class TetrisManager : MonoBehaviour
 
     }
 
+    /// <summary>
+    /// 更新事件:一秒執行約60次
+    /// </summary>
+    private void Update()
+    {
+        ControlTetis();
+    }
 
+    /// <summary>
+    /// 控制俄羅斯方塊
+    /// </summary>
+    private void ControlTetis()
+    {
+        //如果 已經有 目前的俄羅斯方塊 
+        if (currentTetris)
+        {
+            timer += Time.deltaTime;        //計時器累加一禎的時間
+            if (timer >= timeFall)
+            {
+                timer = 0;
+                currentTetris.anchoredPosition -= new Vector2(0, 50);
+            }
+            //KeyCode 列舉(下拉式選單)
+            //或者        ||
+            //按下D 或 右方向 往右50
+            if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                currentTetris.anchoredPosition += new Vector2(50, 0);
+            }
+            //按下A 或 左方向 往左50
+            if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                currentTetris.anchoredPosition -= new Vector2(50, 0);
+            }
+            //按下 W或下方向 逆時針旋轉90度
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                //屬性面板上的 Rotation 必須用eulerAngles 控制
+                currentTetris.eulerAngles += new Vector3(0, 0, 90);
+            }
+
+        }
+
+    }
 
     private void RandomTetris()
     {
@@ -58,18 +112,21 @@ public class TetrisManager : MonoBehaviour
         GameObject current = Instantiate(tetris, traCanvas);
         //GetComponent<任何元件>()
         //<>    指所有類型
-        current.GetComponent<RectTransform>().anchoredPosition = new Vector2(-15,360);
+        current.GetComponent<RectTransform>().anchoredPosition = new Vector2(-60, 360);
 
         //上一個方塊隱藏
         tetris.SetActive(false);
         //隨機取得下一個方塊
         RandomTetris();
 
+        //將生成的俄羅斯方塊 RectTransform 元件儲存
+        currentTetris = current.GetComponent<RectTransform>();
+
     }
 
     private void AddGo()
     {
-        
+
 
 
 
